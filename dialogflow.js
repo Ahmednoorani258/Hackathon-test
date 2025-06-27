83de3d3dde2150bac5a5e8820c91df3a3c549302
ahmednoorani <ahmednoorani258@gmail.com>
2025-06-28 00:45:28 +0500

    test

--- dialogflow.js
+++ dialogflow.js
@@ -4,7 +4,7 @@ const express = require("express")
 const cors = require("cors");
 
 const app = express();
-app.use(express.json())
+app.use(express.json()) 
 app.use(cors());
 
 const PORT = process.env.PORT || 8080;
